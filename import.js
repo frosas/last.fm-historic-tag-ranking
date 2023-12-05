65692ac27601224de97a559d16bfc27f7871e770
Francesc Rosas <francesc@francescrosas.com>
2023-12-05 17:47:28 +0000

    Refactor

--- import.js
+++ import.js
@@ -1,21 +1,24 @@
 import csv from 'csv-parser'
 import fs from 'node:fs'
 
-export function import_data(file) {
-	return new Promise((resolve, reject) => {
-		let results = []
+export async function import_scrobbles(file) {
+	const scrobbles = await new Promise((resolve, reject) => {
+		const scrobbles = []
 		fs.createReadStream(file)
 			.pipe(csv())
-			.on('data', (data) => {
-				results.push(data)
-			})
-			.on('end', () => {
-				resolve(results)
-			})
-			.on('error', (e) => {
-				reject(e)
-			})
+			.on('data', (data) => scrobbles.push(data))
+			.on('end', () => resolve(scrobbles))
+			.on('error', (e) => reject(e))
 	})
+
+	for (const scrobble of scrobbles) {
+		// this removes weird "1970" data from the export, not sure what happened there
+		if (+scrobble.uts < 31536000) continue
+		scrobble.date = new Date(scrobble.uts * 1000)
+		scrobble.year = scrobble.date.getFullYear()
+	}
+
+	return scrobbles
 }
 
 const TAG_PER_ARTIST_FILE_PATH = './tag_per_artist.json'
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { export_tag_per_artist, import_data, import_tag_per_artist } from './import.js'
+import { export_tag_per_artist, import_scrobbles, import_tag_per_artist } from './import.js'
 
 // configuration
-const SCROBBLES_CSV = './scrobbles-Linkplay9-1689688285.csv'
+const SCROBBLES_CSV = './recenttracks-r00z-1701781243.csv'
 const NUMBER_TOP_TAGS = 30
 const FETCH_TAGS = true
 const IGNORED_TAGS = [
@@ -25,37 +25,26 @@ const IGNORED_TAGS = [
 
 async function main() {
 	console.warn('importing local data')
-	let data = await import_data(SCROBBLES_CSV)
+	const scrobbles = await import_scrobbles(SCROBBLES_CSV)
 	const tag_per_artist = import_tag_per_artist()
-
-	console.warn('converting dates')
-	for (let i = 0; i < data.length; i++) {
-		const entry = data[i]
-		entry.date = new Date(entry.uts * 1000)
-		entry.year = entry.date.getFullYear()
-	}
 
 	const already_fetched_artists = new Set()
 
 	const tag_count_per_year = {}
 
 	console.warn('searching tags')
-	for (let i = 0; i < data.length; i++) {
-		const scrobbl = data[i]
-		// this removes weird "1970" data from the export, not sure what happened there
-		if (+scrobbl.uts < 31536000) {
-			continue
-		}
-		let tag = tag_per_artist.get(scrobbl.artist)
+	for (let i = 0; i < scrobbles.length; i++) {
+		const scrobble = scrobbles[i]
+		let tag = tag_per_artist.get(scrobble.artist)
 		if (
 			FETCH_TAGS &&
 			(!tag || IGNORED_TAGS.includes(tag)) &&
 			tag != null &&
-			!already_fetched_artists.has(scrobbl.artist)
+			!already_fetched_artists.has(scrobble.artist)
 		) {
-			console.warn('# Fetching tags for artist: ', scrobbl.artist)
+			console.warn('# Fetching tags for artist: ', scrobble.artist)
 			const tags = await fetch(
-				`https://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist=${scrobbl.artist}&api_key=a014e53e73aba0fde3d38f1c5ec3c12b&format=json`
+				`https://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist=${scrobble.artist}&api_key=a014e53e73aba0fde3d38f1c5ec3c12b&format=json`
 			)
 				.then(async (r) => {
 					const json = await r.json()
@@ -63,7 +52,7 @@ async function main() {
 					return json.toptags.tag
 				})
 				.catch((reason) => {
-					console.error('FAILED TO GET TAG FOR ARTIST: ' + scrobbl.artist)
+					console.error('FAILED TO GET TAG FOR ARTIST: ' + scrobble.artist)
 					console.error('reason: ', JSON.stringify(reason))
 					return []
 				})
@@ -73,11 +62,11 @@ async function main() {
 				if (!IGNORED_TAGS.includes(tag)) break
 			}
 
-			already_fetched_artists.add(scrobbl.artist)
+			already_fetched_artists.add(scrobble.artist)
 			console.warn(tag)
 			if (tag && (tag.toLowerCase() === 'hip hop' || tag.toLowerCase() === 'rap')) tag = 'Hip-Hop'
 			if (tag && tag.toLowerCase() === '8-bit') tag = 'chiptune'
-			tag_per_artist.set(scrobbl.artist, tag)
+			tag_per_artist.set(scrobble.artist, tag)
 			export_tag_per_artist(tag_per_artist)
 		}
 
@@ -86,11 +75,11 @@ async function main() {
 
 		if (IGNORED_TAGS.includes(tag)) continue
 
-		if (!tag_count_per_year[scrobbl.year]) tag_count_per_year[scrobbl.year] = []
-		let tag_in_year = tag_count_per_year[scrobbl.year].find((i) => i.tag === tag)
+		if (!tag_count_per_year[scrobble.year]) tag_count_per_year[scrobble.year] = []
+		let tag_in_year = tag_count_per_year[scrobble.year].find((i) => i.tag === tag)
 		if (!tag_in_year) {
 			tag_in_year = { tag, count: 0 }
-			tag_count_per_year[scrobbl.year].push(tag_in_year)
+			tag_count_per_year[scrobble.year].push(tag_in_year)
 		}
 		tag_in_year.count++
 	}
