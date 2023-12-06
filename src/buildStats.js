9f5cadae5ea1d91f36f1d10a1e188cb3d6eedcd5
Francesc Rosas <francesc@francescrosas.com>
2023-12-06 18:45:53 +0000

    Misc

--- src/buildStats.js
+++ src/buildStats.js
@@ -7,11 +7,18 @@ import { writeFileSync } from 'fs'
 export function buildStats({ artistsTags }) {
 	const artistsByTag = [...artistsTags].reduce((artistsByTag, [artist, tag]) => {
 		const effectiveTag = tag ?? 'unknown'
-		return { ...artistsByTag, [effectiveTag]: [...(artistsByTag[effectiveTag] || []), artist] }
+		const artists = [...(artistsByTag[effectiveTag] ?? []), artist]
+		return { ...artistsByTag, [effectiveTag]: artists }
 	}, {})
+	for (const tagArtists of Object.values(artistsByTag)) {
+		tagArtists.sort((artist1, artist2) => artist1.localeCompare(artist2))
+	}
 	const stats = {
 		tags: Object.keys(artistsByTag),
 		artistsByTag,
+		tagsByPopularity: Object.entries(artistsByTag)
+			.sort(([, tag1Artists], [, tag2Artists]) => tag2Artists.length - tag1Artists.length)
+			.map(([tag, artists]) => ({ tag, artists: artists.length })),
 	}
 	writeFileSync('./stats.json', JSON.stringify(stats, null, 2))
 }
